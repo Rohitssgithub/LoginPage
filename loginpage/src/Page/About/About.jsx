83f959899f7d627f413a97f62ccbd42201779330
rohit <rohitsawant@gmail.com>
2023-10-31 18:26:29 +0530

    changes

--- loginpage/src/Page/About/About.jsx
+++ loginpage/src/Page/About/About.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import LoadingBar from 'react-top-loading-bar';
 
 const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+  const [progress, setProgress] = useState(0);
 
-export default About
+  let [data, setData] = useState([])
+
+  // const getData = async () => {
+  //   let d = await axios.get('https://dummyjson.com/users')
+  //   console.log('d', d)
+  //   setData(d)
+  // }
+
+  const simulateAPICall = () => {
+    setProgress(10);
+    setTimeout(() => {
+      setProgress(100);
+    }, 3000);
+  };
+
+  // useEffect(async () => {
+  //   setProgress(10);
+  //   await getData()
+  //   setProgress(100);
+  // }, [])
+
+  return (
+    <div>
+      <LoadingBar
+        color="#f11946"
+        progress={progress}
+        onLoaderFinished={() => setProgress(0)}
+      />
+      <button onClick={simulateAPICall}>Start API Call</button>
+    </div>
+  );
+};
+
+export default About;
